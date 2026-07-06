2d4f94c5ed4f6677a4c8df3fc01738d3dffa76b8
Eymen-maker <eymennamli@gmail.com>
2026-07-07 00:15:38 +0300

    added the first version of my game
    basic movement system to character/player and a simple enemy design.

---
+++ Older versions /SOURCE CODE NOT .ZIP/scripts/area_2d.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+@onready var character: CharacterBody2D = $"../CHARACTER"
+@onready var area_2d: Area2D = $Area2D
+
+var direction
+var speed = 8000
+var touching_body = 0
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	touching_body = 0
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	touching_body = 1
+
+
+func _process(delta: float) -> void:
+	direction = character.global_position - global_position
+	direction = direction.normalized()
+	
+	velocity = direction * speed * delta * touching_body
+	
+	print(touching_body)
+
+
+	
+	
+	
+	move_and_slide()
---
+++ Older versions /SOURCE CODE NOT .ZIP/scripts/character.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+
+const SPEED = 30000.0
+const JUMP_VELOCITY = -400.0
+var DASH = SPEED / 2
+
+
+func _physics_process(delta: float) -> void:
+
+	var up_down := Input.get_axis("forward","backward")
+	var left_right := Input.get_axis("left","right")
+	
+
+
+	var direction = Vector2(left_right, up_down).normalized()
+
+	velocity = direction * SPEED * delta
+	
+	if Input.is_action_just_pressed("dash"):
+		global_position = global_position + Vector2(left_right * DASH * delta ,up_down * DASH * delta)
+
+	
+
+
+	move_and_slide()
